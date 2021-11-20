69d2f504ce10144c1ac8e2f92979a2798c3e5fbe
Alejandro Gonzalez Alonso <alejandrolegno@gmail.com>
2021-11-20 16:54:31 +0000

    refactor: refactors starfield css

--- components/molecules/hero/hero.tsx
+++ components/molecules/hero/hero.tsx
@@ -10,26 +10,30 @@ import styles from "./hero.module.css";
 const Hero = () => {
   return (
     <div className={styles.hero}>
-      <div className={styles.stars} />
-      <div />
-      <Container center>
-        <Typography.h1 className={classnames(styles.title, styles.glare)}>
-          <span>Hello</span>
-          <span>there!</span>
-        </Typography.h1>
-        <div className={styles.center}>
-          <LightSaber />
-        </div>
-        <div className={classnames(styles.center, styles.profileInformation)}>
-          <Typography.p variant="h2" className={styles.glare}>
-            I am
-          </Typography.p>
-          <ProfileCard />
-        </div>
-      </Container>
+      <div className={styles.background}>
+        <div className={styles.stars} />
+      </div>
+      <div className={styles.content}>
+        <div />
+        <Container center>
+          <Typography.h1 className={classnames(styles.title, styles.glare)}>
+            <span>Hello</span>
+            <span>there!</span>
+          </Typography.h1>
+          <div className={styles.center}>
+            <LightSaber />
+          </div>
+          <div className={classnames(styles.center, styles.profileInformation)}>
+            <Typography.p variant="h2" className={styles.glare}>
+              I am
+            </Typography.p>
+            <ProfileCard />
+          </div>
+        </Container>
 
-      <div className={styles.center}>
-        <ChevronDown className={styles.chevron} size={36} />
+        <div className={styles.center}>
+          <ChevronDown className={styles.chevron} size={36} />
+        </div>
       </div>
     </div>
   );
