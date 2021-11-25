3b47cc6e79a377e972d6a2a18fcf75dff327800d
Alejandro Gonzalez Alonso <alejandrolegno@gmail.com>
2021-11-25 11:20:46 +0000

    refactor: adds icons to skills

--- components/organisms/skills/skills.tsx
+++ components/organisms/skills/skills.tsx
@@ -1,6 +1,23 @@
 import Skill from "components/molecules/skill/skill";
 import Typography from "components/atoms/typography/typography";
 import Divider from "components/atoms/divider/divider";
+import {
+  SiCplusplus,
+  SiCss3,
+  SiDocker,
+  SiGraphql,
+  SiHtml5,
+  SiJavascript,
+  SiJest,
+  SiMongodb,
+  SiNodedotjs,
+  SiPostcss,
+  SiPython,
+  SiReact,
+  SiTypescript,
+  SiVuedotjs,
+} from "react-icons/si";
+import { FiDatabase } from "react-icons/fi";
 
 import styles from "./skills.module.css";
 
@@ -13,15 +30,15 @@ const Skills = () => {
         </Typography.h3>
       </Divider>
       <div className={styles.grid}>
-        <Skill label="HTML5" rate={7.5} />
-        <Skill label="CSS3" rate={7.5} />
-        <Skill label="JavaScript" rate={8} />
-        <Skill label="TypeScript" rate={8} />
-        <Skill label="React" rate={8} />
-        <Skill label="Vue" rate={4.5} />
-        <Skill label="PostCSS" rate={6.5} />
-        <Skill label="GraphQL" rate={6.5} />
-        <Skill label="Jest" rate={5} />
+        <Skill label="HTML5" icon={<SiHtml5 size={64} />} rate={7.5} />
+        <Skill label="CSS3" icon={<SiCss3 size={64} />} rate={7.5} />
+        <Skill label="JavaScript" icon={<SiJavascript size={64} />} rate={8} />
+        <Skill label="TypeScript" icon={<SiTypescript size={64} />} rate={8} />
+        <Skill label="React" icon={<SiReact size={64} />} rate={8} />
+        <Skill label="Vue" icon={<SiVuedotjs size={64} />} rate={4.5} />
+        <Skill label="PostCSS" icon={<SiPostcss size={64} />} rate={6.5} />
+        <Skill label="GraphQL" icon={<SiGraphql size={64} />} rate={6.5} />
+        <Skill label="Jest" icon={<SiJest size={64} />} rate={5} />
       </div>
       <Divider align="start">
         <Typography.h3 className={styles.section}>
@@ -29,17 +46,17 @@ const Skills = () => {
         </Typography.h3>
       </Divider>
       <div className={styles.grid}>
-        <Skill label="Node.js" rate={6} />
-        <Skill label="Python" rate={3} />
-        <Skill label="SQL" rate={5} />
-        <Skill label="MongoDB" rate={5} />
+        <Skill label="Node.js" icon={<SiNodedotjs size={64} />} rate={6} />
+        <Skill label="Python" icon={<SiPython size={64} />} rate={3} />
+        <Skill label="SQL" icon={<FiDatabase size={64} />} rate={5} />
+        <Skill label="MongoDB" icon={<SiMongodb size={64} />} rate={5} />
       </div>
       <Divider align="start">
         <Typography.h3 className={styles.section}>Others</Typography.h3>
       </Divider>
       <div className={styles.grid}>
-        <Skill label="Docker" rate={5} />
-        <Skill label="C++" rate={5} />
+        <Skill label="Docker" icon={<SiDocker size={64} />} rate={5} />
+        <Skill label="C++" icon={<SiCplusplus size={64} />} rate={5} />
       </div>
       <Divider align="start">
         <Typography.h3 className={styles.section}>Languages</Typography.h3>
