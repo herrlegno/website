d7794aeb6e41a21d43371dd5cededc93d8e3e5b7
Alejandro Gonzalez Alonso <alejandrolegno@gmail.com>
2021-11-25 10:32:37 +0000

    feat: adds sections to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
 import type { NextPage } from "next";
 import Hero from "components/organisms/hero/hero";
+import Section from "components/molecules/section/section";
+import Skills from "components/organisms/skills/skills";
 
 const Index: NextPage = () => {
-  return <Hero />;
+  return (
+    <>
+      <header>
+        <Hero />
+      </header>
+      <main>
+        <Section title="About me" />
+        <Section title="Skills">
+          <Skills />
+        </Section>
+        <Section title="Career" />
+      </main>
+    </>
+  );
 };
 
 export default Index;
